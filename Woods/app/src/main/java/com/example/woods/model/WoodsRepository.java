e2457344d4b51795ab8a4da1ad323e4f395b329c
Gnomo000 <danixandrerodrigues@gmail.com>
2022-01-19 13:09:12 +0000

    Update

--- Woods/app/src/main/java/com/example/woods/model/WoodsRepository.java
+++ Woods/app/src/main/java/com/example/woods/model/WoodsRepository.java
@@ -1,4 +1,24 @@
 package com.example.woods.model;
 
+import android.content.Context;
+
+import com.example.woods.model.local.AppDatabase;
+import com.example.woods.model.local.OrdersDao;
+import com.example.woods.model.local.UsersDao;
+import com.example.woods.model.local.WoodsDao;
+
 public class WoodsRepository {
+
+    private final Context context;
+    private WoodsDao woodsDao;
+    private OrdersDao ordersDao;
+    private UsersDao usersDao;
+
+    public WoodsRepository(Context context) {
+        this.woodsDao = AppDatabase.getInstance(context).getWoodsDao();
+        this.ordersDao = AppDatabase.getInstance(context).getOrdersDao();
+        this.usersDao = AppDatabase.getInstance(context).getUsersDao();
+        this.context = context;
+    }
+    
 }
--- Woods/app/src/main/java/com/example/woods/model/remote/WoodsService.java
+++ Woods/app/src/main/java/com/example/woods/model/remote/WoodsService.java
@@ -1,4 +1,14 @@
 package com.example.woods.model.remote;
 
+import com.example.woods.model.Woods;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+
 public interface WoodsService {
+    @GET("woods/")
+    Call<List<Woods>> getAllWoods(@Header("Authorization") String authToken);
 }
